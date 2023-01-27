7c449e28cc60c65b8aba78037bb4358fc3b0747e
Hécate Moonlight <hecate+github@glitchbra.in>
2023-01-27 12:01:23 +0100

    lint

--- src/Effectful/Time.hs
+++ src/Effectful/Time.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeFamilies #-}
+
 {-|
   Module      : Effectful.Time
   Copyright   : © Hécate Moonlight, 2021
@@ -9,43 +10,51 @@
   An effect wrapper around Data.Time for the Effectful ecosystem
 -}
 module Effectful.Time
-  (
-  -- * Time Effect
-    Time(..)
+  ( -- * Time Effect
+    Time (..)
   , UTCTime
   , getCurrentTime
-  -- * Runners
+
+    -- * Runners
   , runCurrentTimeIO
   , runCurrentTimePure
-
   ) where
 
 import Control.Monad.IO.Class
 import Data.Kind
 import Data.Time (UTCTime)
-import Effectful
 import qualified Data.Time as T
+import Effectful
 import Effectful.Dispatch.Dynamic
 
 -- | An effect for getting the current time
 data Time :: Effect where
   CurrentTime :: Time m UTCTime
 
--- |
--- @since 0.0.1.0
+{-|
+@since 0.0.1.0
+-}
 type instance DispatchOf Time = 'Dynamic
 
 -- | Retrieve the current time in your effect stack
-getCurrentTime :: forall (es :: [Effect])
-                . Time :> es => Eff es UTCTime
+getCurrentTime
+  :: forall (es :: [Effect])
+   . Time :> es
+  => Eff es UTCTime
 getCurrentTime = send CurrentTime
 
 -- | The default IO handler
-runCurrentTimeIO :: forall (es :: [Effect]) (a :: Type)
-                  . IOE :> es => Eff (Time : es) a -> Eff es a
+runCurrentTimeIO
+  :: forall (es :: [Effect]) (a :: Type)
+   . IOE :> es
+  => Eff (Time : es) a
+  -> Eff es a
 runCurrentTimeIO = interpret $ \_ CurrentTime -> liftIO T.getCurrentTime
 
 -- | The pure handler, with a static value
-runCurrentTimePure :: forall (es :: [Effect]) (a :: Type)
-                    . UTCTime -> Eff (Time : es) a -> Eff es a
+runCurrentTimePure
+  :: forall (es :: [Effect]) (a :: Type)
+   . UTCTime
+  -> Eff (Time : es) a
+  -> Eff es a
 runCurrentTimePure time = interpret $ \_ CurrentTime -> pure time
